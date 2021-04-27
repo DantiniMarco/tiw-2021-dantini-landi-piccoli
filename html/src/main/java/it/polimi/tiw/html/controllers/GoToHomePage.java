56cd34ccc2ddb86e4b9957e8eb0e64f15a8db143
LuigiPiccoli17 <piccolinexus@gmail.com>
2021-04-27 13:32:24 +0200

    Fix filter

--- html/src/main/java/it/polimi/tiw/html/controllers/GoToHomePage.java
+++ html/src/main/java/it/polimi/tiw/html/controllers/GoToHomePage.java
@@ -44,15 +44,6 @@ public class GoToHomePage extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // If the user is not logged in (not present in session) redirect to the login
-        String loginpath = getServletContext().getContextPath() + "/index.html";
-        HttpSession session = request.getSession();
-        if (session.isNew() || session.getAttribute("user") == null) {
-            response.sendRedirect(loginpath);
-            return;
-        }
-        User user = (User) session.getAttribute("user");
-
         // Redirect to the Home page and add missions to the parameters
         String path = "/WEB-INF/Home.html";
         ServletContext servletContext = getServletContext();
