abb348f206024404b1ec7a24bdb111794ed4f0cd
marcodanti <10603556@polimi.it>
2021-05-08 10:46:46 +0200

    AuctionDAO updated with findOpenAuction

---
+++ html/src/main/java/it/polimi/tiw/html/controllers/GetSearchedAuction.java
@@ -0,0 +1,67 @@
+package it.polimi.tiw.html.controllers;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import it.polimi.tiw.html.beans.User;
+import it.polimi.tiw.html.dao.AuctionDAO;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import it.polimi.tiw.html.beans.Auction;
+
+@WebServlet("/GetSearchedAuction")
+public class GetSearchedAuction extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private Connection connection = null;
+    private TemplateEngine templateEngine;
+
+    public GetSearchedAuction(){super();}
+
+    public void init() throws ServletException {
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+        try {
+            ServletContext context = getServletContext();
+            String driver = context.getInitParameter("dbDriver");
+            String url = context.getInitParameter("dbUrl");
+            String user = context.getInitParameter("dbUser");
+            String password = context.getInitParameter("dbPassword");
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            throw new UnavailableException("Can't load database driver");
+        } catch (SQLException e) {
+            throw new UnavailableException("Couldn't get db connection");
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        HttpSession s = request.getSession();
+        User u = (User) s.getAttribute("utente");
+        AuctionDAO aDAO= new AuctionDAO(connection, u.getIdUser());
+        ArrayList<Auction> choosenList = new ArrayList<Auction>;
+
+
+    }
+
+}
+
--- html/src/main/java/it/polimi/tiw/html/dao/AuctionDAO.java
+++ html/src/main/java/it/polimi/tiw/html/dao/AuctionDAO.java
@@ -1,8 +1,5 @@
 package it.polimi.tiw.html.dao;
 
-import it.polimi.tiw.html.beans.Auction;
-import it.polimi.tiw.html.beans.AuctionStatus;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,29 +7,91 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import it.polimi.tiw.html.beans.Auction;
+import it.polimi.tiw.html.beans.AuctionStatus;
+
+
 public class AuctionDAO {
     private Connection con;
+    private ArrayList<Auction> searchedList;
 
-    public AuctionDAO(Connection con){
-        this.con=con;
+    public AuctionDAO(Connection connection, int idUser) {
+        this.con = connection;
     }
 
-    public List<Auction> findAuctionsByStaus(AuctionStatus status) {
-        List<Auction> auctions = new ArrayList<>();
+    /**
+     * this query returns the list of sorted by date auctions filtered by keyword
+     * @author Marco
+     * @param keyword
+     * @return
+     * @throws SQLException
+     */
+    public ArrayList<Auction> findOpenAuction(String keyword) throws SQLException {
+        ArrayList<Auction> searchedList = null;
 
+        String query = "SELECT idauction, deadline, minraise, initialprice FROM " +
+                "(item JOIN auction ON idauction = iditem ) WHERE item.name LIKE = ? OR " +
+                "item.description LIKE = ? HAVING auction.deadline >= " +
+                "getDate() ORDER BY auction.deadline DESC";
+        ResultSet result = null;
+        PreparedStatement pstatement = null;
         try {
-
-            String query = "SELECT * FROM auction WHERE status=?";
-            PreparedStatement preparedStatement = con.prepareStatement(query);
-            ResultSet result = preparedStatement.executeQuery();
-            while (result.next()) {
-                Auction auction = new Auction();
-
-                auctions.add(auction);
+            pstatement = con.prepareStatement(query);
+            pstatement.setString(1, keyword);
+            pstatement.setString(2, keyword);
+            result = pstatement.executeQuery();
+            if (!result.isBeforeFirst()) // no results
+                return null;
+            else {
+                while (result.next()) {
+                    Auction auction = new Auction();
+                    auction.setInitialPrice(result.getInt("initialprice"));
+                    auction.setMinRaise(result.getInt("minraise"));
+                    auction.setDeadline(result.getDate("deadline"));
+                    auction.setIdAuction(result.getInt("idauction"));
+                    searchedList.add(auction);
+                }
             }
         } catch (SQLException e) {
-
+            e.printStackTrace();
+            throw new SQLException(e);
+        } finally {
+            try {
+                result.close();
+            } catch (Exception e1) {
+                throw new SQLException(e1);
+            }
+            try {
+                pstatement.close();
+            } catch (Exception e2) {
+                throw new SQLException(e2);
+            }
         }
-        return auctions;
+        return searchedList;
     }
-}
+
+    /**
+     * @author Alfredo
+     * @param status
+     * @return
+     */
+        public List<Auction> findAuctionsByStatus(AuctionStatus status) {
+            List<Auction> auctions = new ArrayList<>();
+
+            try {
+
+                String query = "SELECT * FROM auction WHERE status=?";
+                PreparedStatement preparedStatement = con.prepareStatement(query);
+                ResultSet result = preparedStatement.executeQuery();
+                while (result.next()) {
+                    Auction auction = new Auction();
+
+                    auctions.add(auction);
+                }
+            } catch (SQLException e) {
+
+            }
+            return auctions;
+        }
+
+    }
