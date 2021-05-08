7ccaafc23b703731bdd2997618d69aa2e7d243ec
marcodanti <10603556@polimi.it>
2021-05-08 12:51:04 +0200

    added GetSearchedAuction.html

--- html/src/main/java/it/polimi/tiw/html/controllers/GetSearchedAuction.java
+++ html/src/main/java/it/polimi/tiw/html/controllers/GetSearchedAuction.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.UnavailableException;
@@ -17,7 +18,9 @@ import javax.servlet.http.HttpSession;
 
 import it.polimi.tiw.html.beans.User;
 import it.polimi.tiw.html.dao.AuctionDAO;
+import org.apache.commons.text.StringEscapeUtils;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
@@ -53,14 +56,46 @@ public class GetSearchedAuction extends HttpServlet {
         }
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+
+
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         HttpSession s = request.getSession();
-        User u = (User) s.getAttribute("utente");
-        AuctionDAO aDAO= new AuctionDAO(connection);
-        ArrayList<Auction> choosenList = new ArrayList<Auction>();
+        String keyWord = request.getParameter("keyword");
+        //check parameter is present
+        /*if( keyWord == null ){
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "missing parameters");
+            return;
+        }*/
+            User u = (User) s.getAttribute("user");
+        AuctionDAO aDAO = new AuctionDAO(connection);
+        ArrayList<Auction> searchedList;
+            //try{
+            //searchedList = aDAO.findOpenAuction(keyWord);
+            /*
+            if(searchedList.isEmpty()){
+                ServletContext servletContext = getServletContext();
+                final WebContext ctx = new WebContext(request,response,servletContext,request.getLocale());
+                ctx.setVariable("errorMsg", "This keyword is not matching to any open auction");
+                String path = "/index.html";
+                templateEngine.process(path, ctx, response.getWriter());
 
-
+            } else{*/
+            String path = "/WEB-INF/GetSearchedAuction.html";
+            //request.setAttribute("searchedList", searchedList);
+            RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+            dispatcher.forward(request,response);
+            //}
+        //} catch(SQLException e){
+             //   response.sendError(500, "Database access failed");
+           // }
+    }
+    public void destroy(){
+        try{
+            if(connection != null){
+                connection.close();
+            }
+        }catch (SQLException sqle){}
     }
 
 }
