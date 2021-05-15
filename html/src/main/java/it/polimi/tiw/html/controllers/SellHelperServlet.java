a5072bf3a7432c8d3f0111d3505609d0427c97e4
LuigiPiccoli17 <piccolinexus@gmail.com>
2021-05-15 16:10:18 +0200

    Add file management

--- html/src/main/java/it/polimi/tiw/html/controllers/SellHelperServlet.java
+++ html/src/main/java/it/polimi/tiw/html/controllers/SellHelperServlet.java
@@ -11,6 +11,7 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.UnavailableException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
@@ -25,12 +26,13 @@ import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 
 @WebServlet("/SellHelperServlet")
+@MultipartConfig
 public class SellHelperServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private Connection con;
     private TemplateEngine templateEngine;
 
-    public void init() throws ServletException{
+    public void init() throws ServletException {
         ServletContext servletContext = getServletContext();
         con = ConnectionHandler.getConnection(getServletContext());
         ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
@@ -43,7 +45,7 @@ public class SellHelperServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String itemName = request.getParameter("itemName");
-        String itemImage = request.getParameter("itemImage");
+        //String itemImage = request.getParameter("itemImage");
         final Part filePart = request.getPart("itemImage");
         final String fileName = getFileName(filePart);
         System.out.println(fileName);
@@ -58,9 +60,9 @@ public class SellHelperServlet extends HttpServlet {
         AuctionDAO am = new AuctionDAO(con);
         OutputStream out = null;
         InputStream filecontent = null;
-        final PrintWriter writer = response.getWriter();
-
-        if (itemName == null || itemName.isEmpty() || itemImage == null || itemImage.isEmpty() || itemDescription == null || itemDescription.isEmpty()
+        //final PrintWriter writer = response.getWriter();
+        System.out.println(System.getProperty("catalina.home"));
+        if (itemName == null || itemName.isEmpty() || filePart == null || itemDescription == null || itemDescription.isEmpty()
                 || initialPrice_param == null || initialPrice_param.isEmpty() || minRaise_param == null || minRaise_param.isEmpty() || deadline_param == null
                 || deadline_param.isEmpty()) {
             bad_request = 1;
@@ -75,40 +77,41 @@ public class SellHelperServlet extends HttpServlet {
         } catch (Exception e) {
             bad_request = 1;
         }
-        try {
-            out = new FileOutputStream(new File("\\img" + File.separator
-                    + fileName));
-            filecontent = filePart.getInputStream();
+        if (filePart.getSize() > 0) {
+            try {
+                out = new FileOutputStream(new File(System.getProperty("catalina.home") + "\\img" + File.separator
+                        + fileName));
+                filecontent = filePart.getInputStream();
 
-            int read = 0;
-            final byte[] bytes = new byte[1024];
+                int read = 0;
+                final byte[] bytes = new byte[1024];
 
-            while ((read = filecontent.read(bytes)) != -1) {
-                out.write(bytes, 0, read);
-            }
-            writer.println("New file " + fileName + " created at " + "\\img");
-            System.out.println("File{0}being uploaded to {1}");
-        } catch (FileNotFoundException fne) {
-            writer.println("You either did not specify a file to upload or are "
+                while ((read = filecontent.read(bytes)) != -1) {
+                    out.write(bytes, 0, read);
+                }
+                //writer.println("New file " + fileName + " created at " + "\\img");
+                System.out.println("File{0}being uploaded to {1}");
+            } catch (FileNotFoundException fne) {
+            /*writer.println("You either did not specify a file to upload or are "
                     + "trying to upload a file to a protected or nonexistent "
-                    + "location.");
-            writer.println("<br/> ERROR: " + fne.getMessage());
+                    + "location.");*/
+                //writer.println("<br/> ERROR: " + fne.getMessage());
 
-            System.out.println("Problems during file upload. Error: {0}");
-            bad_request = 1;
-        } finally {
-            if (out != null) {
-                out.close();
-            }
-            if (filecontent != null) {
-                filecontent.close();
-            }
-            if (writer != null) {
+                System.out.println("Problems during file upload. Error: {0}");
+                bad_request = 1;
+            } finally {
+                if (out != null) {
+                    out.close();
+                }
+                if (filecontent != null) {
+                    filecontent.close();
+                }
+            /*if (writer != null) {
                 writer.close();
+            }*/
             }
         }
-
-        if(bad_request==1){
+        if (bad_request == 1) {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Parameter/s missing");
         }
 
@@ -123,7 +126,7 @@ public class SellHelperServlet extends HttpServlet {
 
 
         ServletContext servletContext = getServletContext();
-        final WebContext ctx = new WebContext(request,response,servletContext,request.getLocale());
+        final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
         String path = "/WEB-INF/Sell.html";
         templateEngine.process(path, ctx, response.getWriter());
     }
