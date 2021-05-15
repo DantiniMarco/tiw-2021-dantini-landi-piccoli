d4ec856f5ee63d07f9d9a0624adb3324b7a3439c
marcodanti <10603556@polimi.it>
2021-05-15 15:33:36 +0200

    Changes by luigi

--- html/src/main/java/it/polimi/tiw/html/controllers/SellHelperServlet.java
+++ html/src/main/java/it/polimi/tiw/html/controllers/SellHelperServlet.java
@@ -7,19 +7,27 @@ import javax.servlet.ServletException;
 import javax.servlet.UnavailableException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.Part;
+import javax.servlet.http.Part.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Date;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 
 @WebServlet("/SellHelperServlet")
 public class SellHelperServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private Connection con;
 
-    public void init() throws ServletException{
+    public void init() throws ServletException {
         con = ConnectionHandler.getConnection(getServletContext());
     }
 
@@ -27,41 +35,80 @@ public class SellHelperServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String itemName = request.getParameter("itemName");
         String itemImage = request.getParameter("itemImage");
+        final Part filePart = request.getPart("itemImage");
+        final String fileName = getFileName(filePart);
+        System.out.println(fileName);
         String itemDescription = request.getParameter("itemDescription");
         String initialPrice_param = request.getParameter("initialPrice");
         String minRaise_param = request.getParameter("minRaise");
         String deadline_param = request.getParameter("deadline");
-        int bad_request=0;
-        float initialPrice=0;
-        float minRaise =0;
-        Date deadline= null;
+        int bad_request = 0;
+        float initialPrice = 0;
+        float minRaise = 0;
+        Date deadline = null;
         AuctionDAO am = new AuctionDAO(con);
+        OutputStream out = null;
+        InputStream filecontent = null;
+        final PrintWriter writer = response.getWriter();
 
-        if(itemName==null || itemName.isEmpty() || itemImage==null || itemImage.isEmpty() || itemDescription==null || itemDescription.isEmpty()
-        || initialPrice_param==null || initialPrice_param.isEmpty() || minRaise_param==null || minRaise_param.isEmpty() || deadline_param==null
-        || deadline_param.isEmpty()){
-            bad_request=1;
+        if (itemName == null || itemName.isEmpty() || itemImage == null || itemImage.isEmpty() || itemDescription == null || itemDescription.isEmpty()
+                || initialPrice_param == null || initialPrice_param.isEmpty() || minRaise_param == null || minRaise_param.isEmpty() || deadline_param == null
+                || deadline_param.isEmpty()) {
+            bad_request = 1;
         }
 
-        try{
-            initialPrice= Float.parseFloat(initialPrice_param);
+        try {
+            initialPrice = Float.parseFloat(initialPrice_param);
             minRaise = Float.parseFloat(minRaise_param);
             deadline = Date.valueOf(deadline_param);
-        }catch(NumberFormatException e){
-            bad_request=1;
-        }catch(Exception e){
-            bad_request=1;
+        } catch (NumberFormatException e) {
+            bad_request = 1;
+        } catch (Exception e) {
+            bad_request = 1;
+        }
+        try {
+            out = new FileOutputStream(new File("\\img" + File.separator
+                    + fileName));
+            filecontent = filePart.getInputStream();
+
+            int read = 0;
+            final byte[] bytes = new byte[1024];
+
+            while ((read = filecontent.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            writer.println("New file " + fileName + " created at " + "\\img");
+            System.out.println("File{0}being uploaded to {1}");
+        } catch (FileNotFoundException fne) {
+            writer.println("You either did not specify a file to upload or are "
+                    + "trying to upload a file to a protected or nonexistent "
+                    + "location.");
+            writer.println("<br/> ERROR: " + fne.getMessage());
+
+            System.out.println("Problems during file upload. Error: {0}");
+            bad_request = 1;
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+            if (filecontent != null) {
+                filecontent.close();
+            }
+            if (writer != null) {
+                writer.close();
+            }
         }
 
-        if(bad_request==1){
+        if (bad_request == 1) {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Parameter/s missing");
         }
 
         //Forse andrebbe controllato (?)
         int idCreator = (Integer) request.getSession().getAttribute("user");
-        try{
-            am.insertNewAuction(itemName,itemImage, itemDescription, initialPrice, minRaise, deadline, idCreator);
-        }catch (SQLException sqle){
+        try {
+            am.insertNewAuction(itemName, itemImage, itemDescription, initialPrice, minRaise, deadline, idCreator);
+        } catch (
+                SQLException sqle) {
             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Issue from database");
         }
 
@@ -73,14 +120,25 @@ public class SellHelperServlet extends HttpServlet {
         doPost(request, response);
     }
 
+    private String getFileName(final Part part) {
+        final String partHeader = part.getHeader("content-disposition");
+        System.out.println("Part Header = {0}");
+        for (String content : part.getHeader("content-disposition").split(";")) {
+            if (content.trim().startsWith("filename")) {
+                return content.substring(
+                        content.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
+    }
 
     @Override
     public void destroy() {
-        try{
-            if(con!=null){
+        try {
+            if (con != null) {
                 con.close();
             }
-        }catch (SQLException sqle){
+        } catch (SQLException sqle) {
             sqle.printStackTrace();
         }
     }
