203226167f25fe85f683ebb12c26d747ff625b8a
LuigiPiccoli17 <piccolinexus@gmail.com>
2021-05-26 19:54:22 +0200

    Chanegs to js

--- js/src/main/webapp/auctionManagement.js
+++ js/src/main/webapp/auctionManagement.js
@@ -1,7 +1,7 @@
 (function () { // avoid variables ending up in the global scope
 
     // page components
-    var auctionsList, searchForm, auctionDetails, buttonManager, pageOrchestrator = new PageOrchestrator(); // main controller
+    var auctionsList, auctionsListSell, searchForm, auctionDetails, buttonManager, pageOrchestrator = new PageOrchestrator(); // main controller
 
     window.addEventListener("load", () => {
         pageOrchestrator.start(); // initialize the components
@@ -105,7 +105,123 @@
 
     }
 
+    function AuctionListSell(_alert, _listopen, _listopenbody, _listclosed, _listclosedbody, _addauctionform) {
+        this.alert = _alert;
+        this.listopen = _listopen;
+        this.listopenbody = _listopenbody;
+        this.listclosed = _listclosed;
+        this.listclosedbody = _listclosedbody;
+        this.addauctionform = _addauctionform;
+        this.addauctionform.querySelector('button[type="submit"]').addEventListener('click', (e) => {
+            e.preventDefault();
+            var form = e.target.closest("form");
+            if (form.checkValidity()) {
+                var self = this,
+                    idauction = form.querySelector("input[type = 'hidden']").value;
+                makeCall("POST", 'CreateBid', form,
+                    function(req) {
+                        if (req.readyState == 4) {
+                            var message = req.responseText;
+                            if (req.status == 200) {
+                                //add bid
+                                self.show(self.currentAuctionId);
+                            } else {
+                                self.alert.textContent = message;
+                            }
+                        }
+                    }
+                );
+            } else {
+                form.reportValidity();
+            }
+        });
 
+        this.reset = function () {
+            this.listcontainer.style.visibility = "hidden";
+            this.listcontainer.style.display = "none";
+        }
+
+        this.show = function (keyword) {
+            var self = this;
+            makeCall("GET", "SellServlet", null,
+                function (req) {
+                    if (req.readyState == 4) {
+                        var message = req.responseText;
+                        if (req.status == 200) {
+                            self.searchalert.textContent = ""
+                            self.alert.textContent = ""
+                            console.log(req.responseText);
+                            var auctionsToShow = JSON.parse(req.responseText);
+                            console.log(auctionsToShow);
+                            if (auctionsToShow.length == 0) {
+                                self.alert.textContent = "No auctions found!";
+                                return;
+                            }
+                            self.update(auctionsToShow); // self visible by closure
+                        }
+                        else {
+                            self.searchalert.textContent = req.responseText;
+                            self.listcontainer.style.visibility = "hidden";
+                            self.listcontainer.style.display = "none";
+                        }
+                    } else {
+                        self.alert.textContent = message;
+                    }
+                }
+            );
+        };
+
+
+        this.update = function (arrayAuctions) {
+            var row, priceCell, raiseCell, dateCell, itemCell, idAuctionCell, linkcell, linkText, anchor;
+            this.listcontainerbody.innerHTML = ""; // empty the table body
+            // build updated list
+            var self = this;
+            arrayAuctions.forEach(function (auction) { // self visible here, not this
+                row = document.createElement("tr");
+                idAuctionCell = document.createElement("td");
+                idAuctionCell.textContent = auction.idAuction;
+                row.appendChild(idAuctionCell);
+                itemCell = document.createElement("td");
+                itemCell.textContent = auction.itemName;
+                row.appendChild(itemCell);
+                priceCell = document.createElement("td");
+                priceCell.textContent = new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' }).format(auction.initialPrice);
+                row.appendChild(priceCell);
+                raiseCell = document.createElement("td");
+                raiseCell.textContent = new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' }).format(auction.minRaise);
+                row.appendChild(raiseCell);
+                dateCell = document.createElement("td");
+                dateCell.textContent = auction.deadline;
+                row.appendChild(dateCell);
+                linkcell = document.createElement("td");
+                anchor = document.createElement("a");
+                linkcell.appendChild(anchor);
+                linkText = document.createTextNode("Details");
+                anchor.appendChild(linkText);
+                //anchor.auctionid = auction.id; // make list item clickable
+                anchor.setAttribute('auctionid', auction.idAuction); // set a custom HTML attribute
+                anchor.addEventListener("click", (e) => {
+                    // dependency via module parameter
+                    auctionDetails.show(e.target.getAttribute("auctionid")); // the list must know the details container
+                }, false);
+                anchor.href = "#";
+                row.appendChild(linkcell);
+                self.listcontainerbody.appendChild(row);
+            });
+            self.listcontainer.style.visibility = "visible";
+            self.listcontainer.style.display = null;
+        }
+
+        this.autoclick = function (missionId) {
+            var e = new Event("click");
+            var selector = "a[auctionid='" + auctionId + "']";
+            var anchorToClick =
+                (auctionId) ? document.querySelector(selector) : this.listcontainerbody.querySelectorAll("a")[0];
+            if (anchorToClick) anchorToClick.dispatchEvent(e);
+        }
+
+    }
 
     function PersonalMessage(_alert, _messagecontainer) {
         this.alert = _alert;
@@ -332,6 +448,14 @@
                 alertContainer,
                 document.getElementById("id_listcontainer"),
                 document.getElementById("id_listcontainerbody"));
+
+            auctionsListSell = new AuctionListSell(
+                alertContainer,
+                document.getElementById("id_sellopencontainer"),
+                document.getElementById("id_sellopencontainerbody"),
+                document.getElementById("id_sellclosedcontainer"),
+                document.getElementById("id_sellclosedcontainerbody"),
+                document.getElementById("id_addAuctionForm"));
 
 
             searchForm = new SearchAuction(document.getElementById("id_searchauctionform"), alertSearchContainer);
